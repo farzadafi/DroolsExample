e66d653b9aa7b8408da11c95821157cae293eb34
farzad <farzadafi40@gmail.com>
2022-07-03 16:30:24 +0430

    In the name of God!

---
+++ src/main/java/com/farzad/droolsexample/DroolsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.farzad.droolsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsExampleApplication.class, args);
+    }
+
+}
